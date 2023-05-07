d368d74b634c5068bd8b97133af9c1b711503f7d
Jonathan <jonathanmoorevla@gmail.com>
2023-05-07 09:57:17 -0500

    Initial commit, generate User resource

---
+++ db/migrate/20230507145328_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :neighborhood
+      t.string :password_digest
+      t.string :bio
+      t.string :fav_author
+      t.last_login :fav_genre
+      t.datetime :last_login
+      t.integer :phone_num
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
